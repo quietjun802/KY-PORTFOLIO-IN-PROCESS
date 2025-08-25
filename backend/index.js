e86eb0afd62130b4bc5c8d5da3ea9a305f244add
quietjun802 <whdydwns802@gmail.com>
2025-08-25 11:42:29 +0900

    11111

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const dotenv = require("dotenv")
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const PORT = process.env.PORT||3000
+const cors = require('cors')
+const cookieParser = require("cookie-parser")
+dotenv.config()
+
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cookieParser())
+app.use(cors(
+    {
+        origin:process.env.FRONT_ORIGIN,
+        credentials:true
+    }
+))
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("연결 성공~")
+}).catch((error)=>console.log("연결 실패", error))
+
+const userRoutes =require("./routes/user")
+app.use("/api/auth",userRoutes)
+
+app.listen(PORT, ()=>{
+    console.log('Server is running')
+})
+
+app.get("/", (req,res)=>{
+    res.send("Hello Express")
+})
